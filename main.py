2a03fd265210b149454fac8c0dd575d5a5713ced
Florian Vahl <florian@flova.de>
2019-12-30 19:36:37 +0100

    "Little" clean up and documentation commit

--- main.py
+++ main.py
@@ -6,106 +6,164 @@ import imutils
 import math
 
 
-def draw_mask(image, mask, color, opacity=0.5):
-        # Make a colored image
-        colored_image = np.zeros_like(image)
-        colored_image[:, :] = tuple(np.multiply(color, opacity).astype(np.uint8))
+class BoatDetector(object):
+    def __init__(self):
+        # Placeholders
+        self.cap = None
+        self._video_input = ""
+        self._last_frame_dog = None
 
-        # Compose debug image with lines
-        return cv2.add(cv2.bitwise_and(
-            image,  image, mask=255-mask),
-            cv2.add(colored_image*opacity, image*(1-opacity), mask=mask).astype(np.uint8))
+    def set_video_input(self, video_input):
+        self._video_input = video_input
+        self._cap = cv2.VideoCapture(self._video_input)  # "/home/florian/Projekt/BehindTheHorizon/data/.mp4"
 
-cap = cv2.VideoCapture("/home/florian/Projekt/BehindTheHorizon/data/.mp4")
+    def _draw_mask(self, image, mask, color, opacity=0.5):
+            # Make a colored image
+            colored_image = np.zeros_like(image)
+            colored_image[:, :] = tuple(np.multiply(color, opacity).astype(np.uint8))
 
-last_frame = None
+            # Compose debug image with lines
+            return cv2.add(cv2.bitwise_and(
+                image,  image, mask=255-mask),
+                cv2.add(colored_image*opacity, image*(1-opacity), mask=mask).astype(np.uint8))
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    def run_detector_on_video_input(self, video_input=None):
+        if video_input is not None:
+            self.set_video_input(video_input)
 
-    frame = cv2.resize(frame, (1200,800))
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = self._cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Resize frame
+            frame = cv2.resize(frame, (1200,800)) # TODO keep aspect ratio
 
-    canvas = np.zeros_like(gray)
+            # Run detection on frame
+            self.analyse_image(frame, history=True)
 
-    points = np.zeros_like(gray).astype(np.uint8)
+             # Show images
+            cv2.imshow('ROI', self.roi_view)
+            cv2.imshow('ROT', self.rotated)
+            cv2.imshow('ROI MEAN', self.roi_mean)
+            cv2.imshow('SPECTRUM', self.magnitude_spectrum)
+            cv2.imshow('DOG', self.dog)
 
-    width = 5
-    for i in range(0, int(1*gray.shape[1] - width + 1), 10):
-        Z = np.float32(gray[:,i:i + width])
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        # define criteria, number of clusters(K) and apply kmeans()
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        K = 2
-        ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+        # When everything done, release the capture
+        self._cap.release()
+        cv2.destroyAllWindows()
 
-        if label[0] != 1:
-            label = np.invert(label)
+    def analyse_image(self, image, roi_height=10, k_mean_width=5, history=True):
+        # Make grayscale version
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        if (int(np.count_nonzero((label))) == 400):
-            continue
+        # Binary image in which the horizon points are placed
+        points = np.zeros_like(gray)
 
-        point = (i, int(np.count_nonzero((label))))
-        cv2.circle(gray, point, 3, 0, -1)
-        cv2.circle(points, point, 1, 255, -1)
-        canvas[:, i] = label.reshape(gray.shape[0])
+        k_mean_stepsize = 10
+
+        for i in range(0, int(gray.shape[1] - k_mean_width), k_mean_stepsize):
+            # Get vertical image slice as float array
+            Z = np.float32(gray[:, i:i + k_mean_width])
+
+            # K-Means termination settings
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+            # Number of classes
+            K = 2
+            # K-Means calculation
+            ret,label,center = cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+            # Determine which class is the sky
+            if label[0] != 1:
+                # Invert if the sky is not class 1
+                label = np.invert(label)
+
+            # Weired bug fix
+            if (int(np.count_nonzero((label))) == 400):
+                continue
+
+            # Determine how many sky pixels are in the slice and approximate them as the y coordinate
+            point = (i, int(np.count_nonzero((label))))
+
+            # Draw horizon point in map
+            cv2.circle(points, point, 1, 255, -1)    # TODO  use list of points instead
+
+        # Fit a RANSEC like line in the horizon point map  (default params)
+        vx, vy, x0, y0 = cv2.fitLine(np.argwhere(points == 255), cv2.DIST_L1, 0, 0.005, 0.01)
+
+        """ Debug line printout
+        cv2.line(
+            gray,
+            (int(y0 + -500 * vy) , int(x0 + -500 * vx)),
+            (int(y0 + 500 * vy) , int(x0 + 500 * vx)),
+            (0,0,255),
+            2)
+        """
+
+        # Rotate image using imutils TODO do this in opencv
+        rotated = imutils.rotate(image, math.degrees(math.atan(vx/ vy)))
+
+        # Crop roi out of rotated image
+        roi = rotated[
+            max(
+                int(x0 - roi_height // 2),
+                0)
+            :
+            min(
+                int(x0 + roi_height // 2),
+                rotated.shape[0]),
+            :]
+
+        # Get gray roi
+        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+        # Calculate a mean in the vertical direction
+        roi_mean = np.mean(gray_roi, axis=0).astype(np.uint8).reshape(1,1200)
+        # Repeat for viz
+        roi_mean = np.repeat(roi_mean, 60, axis=0)
+
+        # Make fft
+        f = np.fft.fft2(roi_mean)
+        fshift = np.fft.fftshift(f)
+        magnitude_spectrum = (20*np.log(np.abs(fshift))).astype(np.uint8)
+
+        # Calculate difference of gaussians
+        blur_large = cv2.blur(roi_mean, (51,1)).astype(np.float)
+        blur_small = cv2.blur(roi_mean, (31,1)).astype(np.float)
+        dog = blur_small - blur_large
+
+        # Enshure all values are above 0
+        dog[dog < 0] = 0
+
+        # Scale image to uint8 scale
+        dog = dog*255
+
+        # Convert image
+        dog = dog.astype(np.uint8)
+
+        # Get K for the complementary filter
+        K = 0.8
+
+        # Calculate time based low pass using the complementary filter
+        if history and self._last_frame_dog is not None:
+            dog = (self._last_frame_dog * K + dog * (1 - K)).astype(np.uint8)
+
+        # Scale roi view
+        self.roi_view = cv2.resize(roi, (1200,40))
+
+        self.roi_mean = roi_mean
+        self.rotated = rotated
+        self.magnitude_spectrum = magnitude_spectrum
+        self.dog = dog
+
+        if history:
+            self._last_frame_dog = self.dog.copy()
 
 
 
-    canvas = canvas.astype(np.uint8) * 255
-q
-    vx, vy, x0, y0 = cv2.fitLine(np.argwhere(points == 255), cv2.DIST_L1, 0, 0.005, 0.01)  # 2 = CV_DIST_L2
-
-    cv2.line(gray,(int(y0 + -500 * vy) , int(x0 + -500 * vx)), (int(y0 + 500 * vy) , int(x0 + 500 * vx)),(0,0,255),2)
-
-    rotated = imutils.rotate(frame, math.degrees(math.atan(vx/ vy)))
-
-    roi_height = 10
-
-    roi = rotated[max(int(x0 - roi_height // 2), 0) : min(int(x0 + roi_height // 2), rotated.shape[0]), :]
-
-    roi_mean = np.mean(cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY), axis=0).astype(np.uint8).reshape(1,1200)
-
-    roi_mean = np.repeat(roi_mean, 60, axis=0)
-
-    f = np.fft.fft2(roi_mean)
-    fshift = np.fft.fftshift(f)
-    magnitude_spectrum = (20*np.log(np.abs(fshift))).astype(np.uint8)
-
-    blur1 = cv2.blur(roi_mean, (51,1)).astype(np.float)
-
-    blur2 = cv2.blur(roi_mean, (31,1)).astype(np.float)
-
-    dog = blur2 - blur1
-
-    dog[dog < 0] = 0
-
-    dog = dog*255
-
-    dog = dog.astype(np.uint8)
-
-    K = 0.8
-
-    if last_frame is not None:
-        dog = (last_frame * K + dog * (1 - K)).astype(np.uint8)
-
-    roi_view = cv2.resize(roi, (1200,40))
-
-    cv2.imshow('ROI', roi_view)
-    cv2.imshow('ROT', rotated)
-    cv2.imshow('ROI MEAN', roi_mean)
-    cv2.imshow('SPECTRUM', magnitude_spectrum)
-    cv2.imshow('DOG', dog)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    last_frame = dog.copy()
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    bt = BoatDetector()
+    bt.run_detector_on_video_input("/home/florian/Projekt/BehindTheHorizon/data/VID_20180818_063412.mp4")
 
